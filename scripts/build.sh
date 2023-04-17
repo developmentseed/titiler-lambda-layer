ec7f00bcc1288500baecb5f491dd3d0c4ce0df85
vincentsarago <vincent.sarago@gmail.com>
2023-04-18 00:53:32 +0200

    requirements.txt

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 PYTHON_VERSION=$1
-TITILER_VERSION=$2
 
 # Base Image
 docker build \
     --platform linux/amd64 \
     --build-arg PYTHON_VERSION=${PYTHON_VERSION} \
-    --build-arg TITILER_VERSION=${TITILER_VERSION} \
     -t lambgeo/titiler:${TITILER_VERSION} .
